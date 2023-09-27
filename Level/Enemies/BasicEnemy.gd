203ba77baf6f6431e804f1b22462706bcc603abc
richardbonneau <richardbonneau91@gmail.com>
2023-09-27 15:22:14 -0400

    basic ai movement

---
+++ Level/Enemies/BasicEnemy.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+var movement_speed: float = 10
+#@onready var movement_target_position: Vector3 = Vector3(-3.0,0.0,2.0)
+var movement_target_position: Vector3
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+
+func _ready():
+	#	Find the Exit Node
+	var exit_node:Area3D = get_tree().get_nodes_in_group("Exit")[0]
+	movement_target_position = exit_node.global_transform.origin
+	print(movement_target_position)
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 0.5
+	navigation_agent.target_desired_distance = 0.5
+
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(movement_target_position)
+
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
+
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector3 = global_position
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+
+	var new_velocity: Vector3 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * movement_speed
+
+	velocity = new_velocity
+	move_and_slide()
