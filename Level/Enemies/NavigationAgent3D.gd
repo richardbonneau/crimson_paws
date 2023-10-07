7d08e971bb3f07923d716bf9bc2f092effb46359
richardbonneau <richardbonneau91@gmail.com>
2023-10-07 15:43:47 -0400

    move basic enemy nav to navigationAgent3D node

---
+++ Level/Enemies/NavigationAgent3D.gd
@@ -0,0 +1,87 @@
+extends NavigationAgent3D
+
+@onready var parent:CharacterBody3D = self.get_parent()
+@onready var waypointFinder:Node3D = parent.get_node("WaypointFinder")
+
+var target_reached_offset_threshold:float = 1
+var buffer_distance_once_waypoint_reached:float = target_reached_offset_threshold + 1
+var has_waypoint_been_just_reached:bool = false
+
+var start_node:Area3D
+var exit_node:Area3D
+
+var _waypoint_index = 0
+var _waypoints = []
+
+var movement_speed: float = 7
+
+func _ready():
+	#	Get the corners array
+	start_node = get_tree().get_nodes_in_group("Start")[0]
+	_waypoints = start_node.td_maze_corners
+	#	Find the Exit Node
+	exit_node = get_tree().get_nodes_in_group("Exit")[0]
+	_waypoints.append(exit_node)
+	
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	self.path_desired_distance = 0.5
+	self.target_desired_distance = 0.5
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+	# Now that the navigation map is no longer empty, set the movement target.
+	go_to_next_waypoint()
+
+func set_movement_target(movement_target: Vector3):
+	self.set_target_position(movement_target)
+
+func go_to_next_waypoint():
+	var movement_target_position:Vector3
+	if _waypoints.size() > 0:
+		movement_target_position = _waypoints[_waypoint_index].global_transform.origin
+	set_movement_target(movement_target_position)
+
+func is_target_in_range(target_node,threshold):
+	var distance = waypointFinder.global_transform.origin.distance_to(target_node.global_transform.origin)
+	return distance < threshold
+
+func is_waypoint_reached():
+	var current_waypoint = _waypoints[_waypoint_index]
+	return is_target_in_range(current_waypoint,target_reached_offset_threshold)
+
+func is_out_of_last_waypoint_range():
+	var last_waypoint = _waypoints[_waypoint_index - 1]
+	return is_target_in_range(last_waypoint,buffer_distance_once_waypoint_reached)
+
+func _physics_process(delta):
+	if self.is_navigation_finished():
+		return
+	
+	if has_waypoint_been_just_reached == false and is_waypoint_reached():
+		# Make sure we don't crash if we increment the index and run go_to_next_waypoint
+		if _waypoint_index + 1 > _waypoints.size() -1:
+			return
+		
+		has_waypoint_been_just_reached = true
+		_waypoint_index += 1
+		go_to_next_waypoint()
+		
+	
+	if has_waypoint_been_just_reached != false and is_out_of_last_waypoint_range():
+		has_waypoint_been_just_reached = false
+	
+	var current_agent_position: Vector3 = parent.global_position
+	var next_path_position: Vector3 = self.get_next_path_position()
+	
+	var new_velocity: Vector3 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * movement_speed
+	
+	parent.velocity = new_velocity
+	parent.look_at(_waypoints[_waypoint_index].global_transform.origin)
+	parent.move_and_slide()
+
