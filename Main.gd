ad9aebe2505cd88bab69efd9044d3e7fb907af76
richardbonneau <richardbonneau91@gmail.com>
2023-10-10 11:03:11 -0400

    towers shooting outside of range fix

--- Main.gd
+++ Main.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Engine.time_scale = 0.5
+	Engine.time_scale = 0.25
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
