ce6fa40f1da741d6f453633dc7b877985ef2a2f5
richardbonneau <richardbonneau91@gmail.com>
2023-12-15 21:22:25 -0500

    change camera

--- Logic/Player/PlayerCamera.gd
+++ Logic/Player/PlayerCamera.gd
@@ -4,4 +4,4 @@ extends Camera3D
 
 func _process(_delta: float) -> void:
 	self.transform.origin.x = player_to_follow.transform.origin.x
-	self.transform.origin.z = player_to_follow.transform.origin.z
+	self.transform.origin.z = player_to_follow.transform.origin.z + 2
