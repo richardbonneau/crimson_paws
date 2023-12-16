17184cc61064f331396b58fde388d44c6281a826
richardbonneau <richardbonneau91@gmail.com>
2023-12-15 20:45:09 -0500

    upgrade surfaces

--- Logic/Player/PlayerCamera.gd
+++ Logic/Player/PlayerCamera.gd
@@ -2,6 +2,6 @@ extends Camera3D
 
 @export var player_to_follow:CharacterBody3D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	self.transform.origin.x = player_to_follow.transform.origin.x
 	self.transform.origin.z = player_to_follow.transform.origin.z
